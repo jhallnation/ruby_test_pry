6549a9f6260bd846d7e0db44939249107bcf0a4f
Jason Hall <hall.jasondaniel@gmail.com>
2017-02-22 12:20:42 -0700

    how are you

---
+++ ruby_test.rb
@@ -0,0 +1,8 @@
+puts 'how are you doing?'
+input = gets.chomp
+if input == 'good'
+	print 'glad to hear'
+elsif input == 'bad'
+	print 'sorry to hear'
+else print 'sorry i dont want to talk to you anymore'
+end		
