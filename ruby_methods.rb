98e1bf7abdcaf64698986b99dae9a47648d5e3fa
Jason Hall <hall.jasondaniel@gmail.com>
2017-02-27 13:08:45 -0700

    euler q1

---
+++ ruby_methods.rb
@@ -0,0 +1,9 @@
+def baseball_team_list
+  ['rockies', 'angels', 'giants']
+end
+def second_baseball_team_list
+  ['braves', 'astros', 'royals']
+end
+
+p baseball_team_list
+p second_baseball_team_list
--- ruby_test2.rb
+++ ruby_test2.rb
@@ -1,3 +1,17 @@
-'what is your name?'
-name = gets.chomp
-print 'Hi #{name}!'
+num = (1..999).to_a
+sum = Array.new
+
+num.each do |x|
+  if x % 3 == 0 
+  sum << x
+  end
+end
+num.each do |i|
+  if i % 5 == 0 
+  sum << i
+  end
+end
+
+sum = sum.uniq
+
+p sum.inject(:+)
